d503dd3b9e3317d3d42b09c59ae2505ac9029f72
양성호 <sungho@yangseonghoui-MacBookAir.local>
2022-02-15 10:29:10 +0900

    Add : infiniteScroll , checkedBook 컴포넌트 레이아웃 작업 완료

--- components/CheckedBook/CheckedBook.tsx
+++ components/CheckedBook/CheckedBook.tsx
@@ -1,3 +1,58 @@
+import styles from './CheckedBook.module.scss';
+
 export default function CheckedBook() {
-  return <div>CheckedBook</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.contentWrapper}>
+        <div className={styles.leftTextsWrapper}>
+          <h3 className={styles.titleText}>
+            교육 전문가들이<br></br>
+            선정한 도서
+          </h3>
+          <p className={styles.descriptionText}>
+            호두랩스의 교육 전문가들이<br></br>
+            교과 수록 , 초등 필수 독서 등<br></br>
+            아동 교육에 적합한 도서를<br></br>
+            직접 선정해요!
+          </p>
+        </div>
+        <div className={styles.rightContentsWrapper}>
+          <div className={styles.checkedImagesWrapper}>
+            <img
+              className={styles.checkedImage}
+              src="./images/check.png"
+              alt="체크 이미지"
+            />
+            <img
+              className={styles.checkedImage}
+              src="./images/check.png"
+              alt="체크 이미지"
+            />
+            <img
+              className={styles.checkedImage}
+              src="./images/check.png"
+              alt="체크 이미지"
+            />
+          </div>
+          <div className={styles.cardImagesWrapper}>
+            <img
+              className={styles.BookImage}
+              src="./images/img_book_1.png"
+              alt="책 이미지1"
+            />
+            <img
+              className={styles.BookImage}
+              src="./images/img_book_2.png"
+              alt="책 이미지2"
+            />
+            <img
+              className={styles.BookImage}
+              src="./images/img_book_3.png"
+              alt="책 이미지3"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- components/InfiniteScrollBooks/InfiniteScrollBooks.tsx
+++ components/InfiniteScrollBooks/InfiniteScrollBooks.tsx
@@ -1,3 +1,70 @@
+import styles from './InfiniteScrollBooks.module.scss';
+
+const scrollBookImages = [
+  {
+    src: './images/scrollBookImg1.png',
+    alt: '스크롤 북 이미지1',
+  },
+  {
+    src: './images/scrollBookImg2.png',
+    alt: '스크롤 북 이미지2',
+  },
+  {
+    src: './images/scrollBookImg3.png',
+    alt: '스크롤 북 이미지3',
+  },
+  {
+    src: './images/scrollBookImg4.png',
+    alt: '스크롤 북 이미지4',
+  },
+  {
+    src: './images/scrollBookImg5.png',
+    alt: '스크롤 북 이미지5',
+  },
+  {
+    src: './images/scrollBookImg6.png',
+    alt: '스크롤 북 이미지6',
+  },
+  {
+    src: './images/scrollBookImg7.png',
+    alt: '스크롤 북 이미지7',
+  },
+  {
+    src: './images/scrollBookImg8.png',
+    alt: '스크롤 북 이미지8',
+  },
+  {
+    src: './images/scrollBookImg9.png',
+    alt: '스크롤 북 이미지9',
+  },
+];
+
 export default function InfiniteScrollBooks() {
-  return <div>InfiniteScrollBooks</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.contentContainer}>
+        <div className={styles.topTextWrapper}>
+          <h3 className={styles.titleText}>
+            입학 전 1000권 읽기!<br></br>
+            땅콩스쿨로 도전해보세요.
+          </h3>
+          <p className={styles.descriptionText}>
+            매달 새로운 32권의 어린이 베스트 셀러를 만날 수 있어요.<br></br>
+            독서를 통해 배움의 즐거움을 알아가 보세요.
+          </p>
+        </div>
+        <div className={styles.InfiniteScrollWrapper}>
+          {scrollBookImages.map((e) => {
+            return (
+              <img
+                className={styles.scrollBookImages}
+                src={e.src}
+                alt={e.alt}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
