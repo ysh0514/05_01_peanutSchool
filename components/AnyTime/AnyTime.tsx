cb7c50ecf0899034f4d0874a1e7a916e541b86c1
leenawon <nawon5154@gmail.com>
2022-02-15 11:49:47 +0900

    Add - AnyTime component 기본 구조 작성

--- components/AnyTime/AnyTime.tsx
+++ components/AnyTime/AnyTime.tsx
@@ -1,5 +1,25 @@
 import styles from "./AnyTime.module.scss";
 
 export default function AnyTime() {
-  return <div className={styles.active}>AnyTime</div>;
+  return (
+    <section className={styles.anyTimeContainer}>
+      <div className={styles.anyTimeWrapper}>
+        <h1>
+          실시간 방송 형태로 제공되는 
+          <br /> 
+          신개념 책 읽기
+        </h1>
+        <p>
+          방송 중이라면 언제든 접속하세요. 
+          <br />
+          수강신청, 수업예약 없이 편하게 수업참여가 가능해요.
+        </p>
+      </div>
+      <img 
+        className={styles.anyTimeImage} 
+        src="/images/anytime_image.png" 
+        alt="태블릿 모바일 이미지"
+      />
+    </section>
+  );
 }
