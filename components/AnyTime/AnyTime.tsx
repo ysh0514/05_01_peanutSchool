19d5f21834427e759a56625810046ebd3b3e1e5c
leenawon <nawon5154@gmail.com>
2022-02-16 00:39:04 +0900

    Add - AnyTime component 글자 애니메이션 추가

--- components/AnyTime/AnyTime.tsx
+++ components/AnyTime/AnyTime.tsx
@@ -1,15 +1,27 @@
+import { useEffect, useState } from 'react';
+import { InViewProps } from '../../utils/inViewType';
 import styles from "./AnyTime.module.scss";
 
-export default function AnyTime() {
+export default function AnyTime({ wRef, wInView }: InViewProps) {
+  const [isAnimation, setIsAnimation] = useState(false);
+
+  useEffect(() => {
+    if (wInView) {
+      setIsAnimation(true);
+    } else {
+      setIsAnimation(false);
+    }
+  }, [wInView]);
+
   return (
-    <section className={styles.anyTimeContainer}>
-      <div className={styles.anyTimeWrapper}>
-        <h1>
+    <section className={styles.anyTimeContainer} ref={wRef}>
+      <div className={!isAnimation ?  styles.anyTimeWrapper : styles.textTransition}>
+        <h1 className={!isAnimation ? styles.title : styles.titleTransform}>
           실시간 방송 형태로 제공되는 
           <br /> 
           신개념 책 읽기
         </h1>
-        <p>
+        <p className={!isAnimation ? styles.text : styles.textTransform}>
           방송 중이라면 언제든 접속하세요. 
           <br />
           수강신청, 수업예약 없이 편하게 수업참여가 가능해요.
