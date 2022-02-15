0bed4dc1c3711bc9b95ea3ddf2d198ec6d145d52
gilpop8663 <wolfye@naver.com>
2022-02-15 11:54:07 +0900

    Add - Bookmarks, Footer , Introduction , SlideComments 파일과 css 파일  추가

--- components/Bookmarks/Bookmarks.tsx
+++ components/Bookmarks/Bookmarks.tsx
@@ -1,3 +1,80 @@
+import styles from './Bookmarks.module.scss';
+
+const messages = [
+  '휴대폰이나 태블릿으로\n시간이나 장소 관계없이\n편하게 수업을 들어보세요.',
+  '화면에 그림도 그려보고,\n읽은 책으로 선생님과\n퀴즈도 풀어보는 양방향\n수업을 경험해보세요.',
+  '친숙한 캐릭터와\n목소리로 수업에 대한\n부담을 낮춰보세요.',
+];
+
 export default function Bookmarks() {
-  return <div>Bookmarks</div>;
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>땅콩스쿨만의 특징</h2>
+      <ul className={styles.cardWrapper}>
+        <li className={styles.card}>
+          <img
+            className={styles.leftString}
+            src="/images/string_left.png"
+            alt="끈"
+          />
+          <img
+            className={styles.cardImage}
+            src="/images/bookmarks.png"
+            alt="북마크 카드"
+          />
+          <div className={styles.content}>
+            <img
+              className={styles.icon}
+              src="/images/videoclass_icon.png"
+              alt="비디오 아이콘"
+            />
+            <span className={styles.cardTitle}>화상 수업</span>
+            <div className={styles.description}>{messages[0]}</div>
+          </div>
+        </li>
+        <li className={styles.card}>
+          <img
+            className={styles.centerString}
+            src="/images/string_center.png"
+            alt="끈"
+          />
+          <img
+            className={styles.cardImage}
+            src="/images/bookmarks.png"
+            alt="북마크 카드"
+          />
+          <div className={styles.content}>
+            <img
+              className={styles.icon}
+              src="/images/bidirectional_icon.png"
+              alt="양방향 아이콘"
+            />
+            <span className={styles.cardTitle}>양방향</span>
+            <div className={styles.description}>{messages[1]}</div>
+          </div>
+        </li>
+        <li className={styles.card}>
+          <img
+            className={styles.rightStirng}
+            src="/images/string_right.png"
+            alt="끈"
+          />
+          <img
+            className={styles.cardImage}
+            src="/images/bookmarks.png"
+            alt="북마크 카드"
+          />
+          <div className={styles.content}>
+            <img
+              className={styles.icon}
+              src="/images/character_icon.png"
+              alt="캐릭터 아이콘"
+            />
+            <span className={styles.cardTitle}>캐릭터</span>
+            <div className={styles.description}>{messages[2]}</div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 }
--- components/Introduction/Introduction.tsx
+++ components/Introduction/Introduction.tsx
@@ -1,3 +1,13 @@
+import styles from './Introduction.module.scss';
+
+const message = `실시간(LIVE)으로, 아이들이 좋아하는 캐릭터 선생님을 통해,\n친구들과 함께 창의독서, 퀴즈 등을 재미있게 즐길 수 있는 온라인 서비스입니다.`;
+
 export default function Introduction() {
-  return <div>Introduction</div>;
+  return (
+    <div className={styles.container}>
+      <img className={styles.logo} src="/images/logo.png" alt="회사 로고" />
+      <h2 className={styles.title}>땅콩스쿨이란?</h2>
+      <div className={styles.message}>{message}</div>
+    </div>
+  );
 }
